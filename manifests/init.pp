be94ee7ce5fda58bba492a57771ab5108b2f1b81
openlock <an.grigoriev84@gmail.com>
2016-03-21 14:39:18 +0300

    Adding all files

---
+++ manifests/init.pp
@@ -0,0 +1,78 @@
+# == Class: jdk_oracle
+#
+# Installs the Oracle Java JDK, from the Oracle servers
+#
+# === Parameters
+#
+# [*version*]
+#   String.  Java Version to install
+#   Defaults to <tt>8</tt>.
+#
+# [*version_update*]
+#   String.  Java Version Update to install
+#   Defaults to <tt>Defaults based on major version</tt>.
+#
+# [*version_build*]
+#   String.  Java Version build to install
+#   Defaults to <tt>Defaults based on major version</tt>.
+#
+# [* java_install_dir *]
+#   String.  Java Installation Directory
+#   Defaults to <tt>/opt</tt>.
+#
+# [* use_cache *]
+#   Boolean.  Optionally host the installer file locally instead of fetching it each time (for faster dev & test)
+#   The puppet cache flag is for faster local vagrant development, to
+#   locally host the tarball from oracle instead of fetching it each time.
+#   Defaults to <tt>false</tt>.
+#
+# [* platform *]
+#   String.  The platform to use
+#   Defaults to <tt>x64</tt>.
+#
+# [* jce *]
+#   Boolean.  Optionally install Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files
+#   Defaults to <tt>false</tt>.
+#
+# [* default_java *]
+#   Boolean.  If the installed java version is linked as the default java, javac etc...
+#   Defaults to <tt>true</tt>.
+#
+# [* ensure *]
+#   String.  Specifies if jdk should be installed or absent
+#   Defaults to <tt>installed</tt>.
+#
+class jdk_oracle (
+  $version        = hiera('jdk_oracle::version',        '8' ),
+  $version_update = hiera('jdk_oracle::version_update', 'default' ),
+  $version_build  = hiera('jdk_oracle::version_build',  'default' ),
+  $install_dir    = hiera('jdk_oracle::install_dir',    '/opt' ),
+  $use_cache      = hiera('jdk_oracle::use_cache',      false ),
+  $cache_source   = 'puppet:///modules/jdk_oracle/',
+  $platform       = hiera('jdk_oracle::platform',       'x64' ),
+  $jce            = hiera('jdk_oracle::jce',            false ),
+  $default_java   = hiera('jdk_oracle::default_java',   true ),
+  $create_symlink = hiera('jdk_oracle::create_symlink', true ),
+  $ensure         = 'installed',
+  $jmxremote_stat = hiera('jdk_oracle::jmxremote_stat', true ),
+  $jmxremote_pass = hiera('jdk_oracle::jmxremote_pass', '#763meheto' ),
+  ) {
+
+
+  if ! defined(Package['wget']) {
+    package { 'wget':
+      ensure =>  present,
+    }
+  }
+
+  if ! defined(Package['unzip']) {
+    package { 'unzip':
+      ensure =>  present,
+    }
+  }
+
+  anchor { 'jdk_oracle::begin': } ->
+  class { '::jdk_oracle::install': } ->
+  anchor { 'jdk_oracle::end':}
+
+}
